6cc3b29c6a217496eef3b51fee892ab9d36289af
Mustafa-Dursun <dursunmustafainfo@gmail.com>
2024-01-30 17:05:43 +0300

    onat

--- index.php
+++ index.php
@@ -10,12 +10,16 @@
 
     <div class="gallery">
         <div class="gallery-container">
-            <img  class="gallery-item gallery-item-1" src="images/image1.jpg" data-index="1">
-            <img  class="gallery-item gallery-item-2" src="images/image2.jpg" data-index="2">
-            <img  class="gallery-item gallery-item-3" src="images/image3.jpg" data-index="3">
-            <img  class="gallery-item gallery-item-4" src="images/image4.jpg" data-index="4">
-            <img  class="gallery-item gallery-item-5" src="images/image5.jpg" data-index="5">
-            <img  class="gallery-item gallery-item-6" src="images/image5.jpg" data-index="6">
+            <img  class="gallery-item gallery-item-1" src="images/aile-birlesimi-vizesi.webp" data-index="1">
+            <img  class="gallery-item gallery-item-2" src="images/apostil-onayli-tercume.webp" data-index="2">
+            <img  class="gallery-item gallery-item-3" src="images/denklik-danismanligi.webp" data-index="3">
+            <img  class="gallery-item gallery-item-4" src="images/diploma-tercume.webp" data-index="4">
+            <img  class="gallery-item gallery-item-5" src="images/noter-onayli-tercume.webp" data-index="5">
+            <img  class="gallery-item gallery-item-6" src="images/schengen-vizesi.webp" data-index="6">
+            <img  class="gallery-item gallery-item-7" src="images/transkript-tercume.webp" data-index="7">
+            <img  class="gallery-item gallery-item-8" src="images/ulusal-vize.webp" data-index="8">
+            <img  class="gallery-item gallery-item-9" src="images/yeminli-tercume-tercuman.webp" data-index="9">
+            <img  class="gallery-item gallery-item-10" src="images/yok-denklik.webp" data-index="10">
         </div>
         <div class="gallery-controls"></div>
     </div>
