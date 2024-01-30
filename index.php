1198380368417dd0965cd154ac1d6b80e98276e4
Mustafa-Dursun <dursunmustafainfo@gmail.com>
2024-01-30 16:49:31 +0300

    ilk

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Carausel</title>
+</head>
+<body>
+
+    <div class="gallery">
+        <div class="gallery-container">
+            <img  class="gallery-item gallery-item-1" src="images/image1.jpg" data-index="1">
+            <img  class="gallery-item gallery-item-2" src="images/image2.jpg" data-index="2">
+            <img  class="gallery-item gallery-item-3" src="images/image3.jpg" data-index="3">
+            <img  class="gallery-item gallery-item-4" src="images/image4.jpg" data-index="4">
+            <img  class="gallery-item gallery-item-5" src="images/image5.jpg" data-index="5">
+            <img  class="gallery-item gallery-item-6" src="images/image5.jpg" data-index="6">
+        </div>
+        <div class="gallery-controls"></div>
+    </div>
+
+
+    <script src="app.js"></script>
+    
+</body>
+</html>
